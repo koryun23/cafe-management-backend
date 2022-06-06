58f9d8b345d643b14e8aab402d8c47768aa0e675
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-06-07 00:27:32 +0400

    project setup

--- src/main/java/com/cafe/Main.java
+++ src/main/java/com/cafe/Main.java
@@ -1,4 +1,12 @@
 package com.cafe;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
 public class Main {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
+    }
 }
