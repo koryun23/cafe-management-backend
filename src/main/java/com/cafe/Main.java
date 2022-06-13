9360b0def5345d4d55374b5da177467982c15ffb
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-06-14 00:54:14 +0400

    minor changes

--- src/main/java/com/cafe/Main.java
+++ src/main/java/com/cafe/Main.java
@@ -7,7 +7,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
