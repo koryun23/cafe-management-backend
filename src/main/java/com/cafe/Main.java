11f7097427f404b2283057f88c9b6aad996c6888
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-08-14 13:24:29 +0400

    removing redundant user creation statements from the Main class

--- src/main/java/com/cafe/Main.java
+++ src/main/java/com/cafe/Main.java
@@ -20,34 +20,5 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
-
-        UserFacade userFacade = context.getBean(UserFacade.class);
-        JwtService jwtService = context.getBean(JwtService.class);
-//
-//        System.out.println(userFacade.registerUser(new UserRegistrationRequestDto(
-//                "ADMIN",
-//                "ADMIN",
-//                "John",
-//                "Williams",
-//                List.of(UserRoleType.MANAGER)
-//        )));
-//        userFacade.registerUser(new UserRegistrationRequestDto(
-//                "mary21",
-//                "pwd21",
-//                "Mary",
-//                "Smith",
-//                List.of(UserRoleType.WAITER)
-//        ));
-
-        System.out.println("---------------------------------");
-        System.out.println("ADMIN");
-        System.out.println(jwtService.createToken("ADMIN", List.of(UserRoleType.MANAGER), new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)));
-        System.out.println("---------------------------------");
-        System.out.println("mary21");
-        System.out.println(jwtService.createToken("mary21", List.of(UserRoleType.WAITER), new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)));
-        System.out.println("---------------------------------");
-        System.out.println("emily31");
-        System.out.println(jwtService.createToken("emily31", List.of(UserRoleType.WAITER), new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)));
-        System.out.println("---------------------------------");
     }
 }
